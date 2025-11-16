01f6ed8d73ca1f7065254b494ff46c62e56ddb52
Milan Singh Chouhan <mchouhan@lumnenore.com>
2025-11-16 22:16:34 +0530

    name fix

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -60,7 +60,7 @@ export default function Home() {
       const data = await response.json();
       setUserDetails({
         id: data.id,
-        userName: data.userName,
+        userName: data.name,
         age: data.age,
         gender: data.gender,
         email: data.email,
